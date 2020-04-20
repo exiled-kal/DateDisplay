d639522b0ea74668f3fdafd53c6e87fafe092079
kaldne <tseringkalden1964@gmail.com>
2020-04-19 22:50:36 -0700

    added changes

---
+++ src/test/java/com/tseringkalden/datedisplay/DateDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tseringkalden.datedisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateDisplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
